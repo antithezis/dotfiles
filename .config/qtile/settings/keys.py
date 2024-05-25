b9bbf0d5060e5c937aeb15bc3fcdfbe0338ec7e5
antithezis <juandbm2001@gmail.com>
2024-05-25 16:17:31 +0000

    Add keymap for lock screen

--- .config/qtile/settings/keys.py
+++ .config/qtile/settings/keys.py
@@ -24,6 +24,8 @@ keys = [Key(key[0], key[1], *key[2:]) for key in [
     ([mod, "shift"], "l", lazy.layout.grow()),
     ([mod, "shift"], "h", lazy.layout.shrink()),
 
+    (['control', 'mod1'], "l", lazy.spawn("betterlockscreen -l blur")),
+
     # Toggle floating
     ([mod, "shift"], "f", lazy.window.toggle_floating()),
 
